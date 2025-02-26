e05f3a4d6b3df40c075cb53574e59f341de47547
JOÃO VICTOR SANTOS DE MORAES <25132878@CFP127ALUNO.LOCAL>
2025-02-26 11:24:56 -0300

    metod get and set beging with calc of IMC

--- src/br/senai/sp/jandira/clinica/Clinica.java
+++ src/br/senai/sp/jandira/clinica/Clinica.java
@@ -8,34 +8,31 @@ public class Clinica {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World ahhhhhhhhhhhhhhhhhh");
-		
 		// Criando um objeto paciente
 		Paciente p1 = new Paciente();
-		p1.nome = "Ana Maria Braga";
+		p1.setNome("Jó");
 		p1.dataNascimento = LocalDate.of(2003, 7, 15); 
-		p1.peso = 51;
-		p1.altura = 1.65;
+		p1.setPeso(51);
+		p1.setAltura(1.65); 
 		p1.telefone = "(11) 4002-8922";
 		
 		Paciente p2 = new Paciente();
-		p2.nome = "João";
+		p2.setNome("João");
 		p2.dataNascimento = LocalDate.of(2005, 2, 28);
-		p2.peso = 68;
-		p2.altura = 1.72;
+		p2.setPeso(64);
+		p2.setAltura(1.72);
 		p2.telefone = "(11) 9464-8451";
 		
 		//exibindo  a referência dos objetos
 		System.out.println(p1);
 		System.out.println(p2);
-		
 		Paciente p3 = p1;
-		
 		System.out.println(p3);
 		
 		//exibindo os dados dos objetos
 		p1.mostrarFichaDoPaciente();
 		p2.mostrarFichaDoPaciente();
+	
 	}
 	
 	
--- src/br/senai/sp/jandira/clinica/model/Paciente.java
+++ src/br/senai/sp/jandira/clinica/model/Paciente.java
@@ -1,22 +1,52 @@
+/*
+ * Programandor: João Moraes
+ * 26/02/2025
+ * Empresa: SENAI
+ */
+
 package br.senai.sp.jandira.clinica.model;
 
 import java.time.LocalDate;
 
 public class Paciente {
 
-	public String nome;
+	private String nome;
 	public LocalDate dataNascimento;
-	public int peso;
-	public double altura;
+	private int peso;
+	private double altura;
 	public String telefone;
+	private double imc;
+	
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	
+	public void setAltura(double altura) {
+		if (altura >= 0) {
+		this.altura = altura;
+		} else {
+			System.out.println("A altura do(a) paciente " + nome + " esta inválida./nA altura deve ser maior ou igual a zero.");
+		}
+	}
+	
+	public void setNome(String nome) {
+
+		if(nome.length() >= 3) {
+			this.nome = nome;
+		} else {
+			System.out.println("O nome " + nome + " não é válido!\nO nome deve conter pelo menos 3 caracteres.");
+		}
+		
+	}
 	
 	public void calcularIdade() {
 		
 	}
 	
-	public void calcularImc() {
+	private double calcularImc() {
 		
-		
+		imc = peso / Math.pow(altura, 2);
+		return imc;
 	}
 		
 	public void classificarImc() {
@@ -31,8 +61,9 @@ public class Paciente {
 		System.out.println("==================");
 		System.out.println("Nome: " + nome);
 		System.out.println("Peso: " + peso);
-		System.out.println("Telefone: " + telefone);
 		System.out.println("Altura: " + altura);
+		System.out.println("IMC: " + String.format("%.2f", calcularImc()));
+		System.out.println("Telefone: " + telefone);
 		System.out.println("==================");
 		
 	}
